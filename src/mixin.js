d749f97e95383c467faa2fcc450a723c9b94d7e1
tertek <ekin.tertemiz@swisstph.ch>
2024-03-01 08:05:53 +0100

    Remove broken toasts. Fix #26

--- src/mixin.js
+++ src/mixin.js
@@ -3,12 +3,13 @@ export default {
     methods: {
        
      toast(msg, title, variant) {
-        this.$bvToast.toast(msg, {
+        /* this.$bvToast.toast(msg, {
             title: title,
             variant: variant,
             autoHideDelay: 250,
             noCloseButton: true
-        })
+        }) */
+        console.log(`${title}: ${msg}`)
      }}
      
  }
